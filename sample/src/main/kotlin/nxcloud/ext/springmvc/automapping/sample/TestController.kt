b031f0d812f3ee81374b665729cf402d80e377b3
Sunshow <sunshow@gmail.com>
2023-01-16 23:07:01 +0800

    增加几个测试映射

--- sample/src/main/kotlin/nxcloud/ext/springmvc/automapping/sample/TestController.kt
+++ sample/src/main/kotlin/nxcloud/ext/springmvc/automapping/sample/TestController.kt
@@ -16,4 +16,19 @@ class TestController {
         println("testVoid")
     }
 
+
+    @RequestMapping("/test1")
+    fun test1(param: Param): String {
+        return "$param"
+    }
+
+    @RequestMapping("/test2")
+    fun test2(a: String, b: Int): String {
+        return a
+    }
+
+    data class Param(
+        val a: String,
+        val b: Int,
+    )
 }
